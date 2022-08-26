99ada9a8eac27a9575b15d5e2527d54d19870cfe
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-08-26 18:14:43 -0300

    Error resolutions

--- src/components/Footer/Footer.elements.tsx
+++ src/components/Footer/Footer.elements.tsx
@@ -5,7 +5,7 @@ export const FooterContainer = styled.footer`
   display: grid;
   grid-template-columns: 1fr 1fr 2fr;
   width: 100%;
-  height: auto;
+  height: 100%;
   bottom: 0;
   align-content: center;
   position: relative;
